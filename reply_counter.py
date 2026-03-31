35e4d5f4e2c2888275c2c4192a8cd199b276d574
KitMage.com <admin@kitmage.com>
2026-03-31 15:34:46 -0500

    Remove unused headers from reply_counter.py

--- reply_counter.py
+++ reply_counter.py
@@ -4,11 +4,7 @@ import requests
 
 HUBSPOT_TOKEN = os.environ.get("HUBSPOT_TOKEN")
 LIST_ID = 677  # HubSpot segment/list ID
+CAMPAIGN_ID = "6afccccd-1f8b-4036-ba17-3eea85f23a05"
 PROPERTY_NAME = "do_not_send_pci"
 
 BASE_URL = "https://api.hubapi.com"
-
-headers = {
-    "Authorization": f"Bearer {HUBSPOT_TOKEN}",
-    "Content-Type": "application/json",
-}
